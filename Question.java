57f9dc59741af3fd0fb77979c411e4171fd2c304
KJohnson82 <rojohnson@gmail.com>
2021-05-19 16:07:02 -0500

    Update Question.java

--- Question.java
+++ Question.java
@@ -1,39 +1,29 @@
+package com.codecademy.unqoute;
 
-public class Question {
-	
-	int correctAnswer;
-	int playerAnswer;
-	int imageId;
-	
-	String questionText;
-	String answer0, answer1, answer2, answer3;
-	
-	
-	public Question(int imageIdentifier, String questionString, String answerZero, String answerOne, String answerTwo, 
-			String answerThree, int correctAnswerIndex) {
-		imageId = imageIdentifier;
-		questionText = questionString;
-		answer0 = answerZero;
-		answer1 = answerOne;
-		answer2 = answerTwo;
-		answer3 = answerThree;
-		correctAnswer = correctAnswerIndex;
-		playerAnswer = -1;
-		
-	}
-	
-	public boolean isCorrect() {
-		if (playerAnswer == correctAnswer) {
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
-	
-	
-	
-	
-	
+class Question {
+
+
+    int correctAnswer, playerAnswer, imageId;
+    public String questionText;
+    public String answer0;
+    public String answer1;
+    public String answer2;
+    public String answer3;
+
+    public Question(int imageId, String questionText, String answer0, String answer1, String answer2, String answer3, int correctAnswer) {
+        this.imageId = imageId;
+        this.questionText = questionText;
+        this.answer0 = answer0;
+        this.answer1 = answer1;
+        this.answer2 = answer2;
+        this.answer3 = answer3;
+        this.correctAnswer = correctAnswer;
+        playerAnswer = -1;
+    }
+
+    public boolean isCorrect() {
+        return correctAnswer == playerAnswer;
+    }
+
 
 }
